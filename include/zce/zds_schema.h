d87f3e12612c01e293d4d501d2f3de7788bc1239
ymwang78 <wangym@gmail.com>
2023-09-22 12:25:50 +0800

    add pack/unpack string

--- include/zce/zds_schema.h
+++ include/zce/zds_schema.h
@@ -6,4 +6,7 @@ namespace zdp
 
 	int ZCE_API zds_unpack(zce_int64* val, zce_byte* buf, zce_int32 size);
 
+	int ZCE_API zds_pack(zce_byte* buf, zce_int32 size, const std::string& val);
+
+	int ZCE_API zds_unpack(std::string& val, const zce_byte* buf, zce_int32 size);
 }
