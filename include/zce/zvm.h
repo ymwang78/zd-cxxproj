9e88c4a04263014b655ee73c1a9054af10a11c65
wangym <wangym@gmail.com>
2023-12-16 22:03:17 +0800

    add builtin

--- include/zce/zvm.h
+++ include/zce/zvm.h
@@ -31,6 +31,25 @@ public:
     //    const std::function<void(int error_code, const zce_dblock& retdata)>& response);
 
     template<typename T>
+    int rpc_call_builtin(const zce_smartptr<zce_object>& vmptr,
+        const std::string& method,
+        const T& t,
+        const std::function<void(int error_code, const zce_dblock& retdata)>& response) {
+        zce_dblock dblock;
+        int ret = zdp::zds_pack_builtin(0, 0, t, 0);
+        if (ret < 0)
+            return ret;
+        ZCE_MBACQUIRE(dblock, ret);
+        if (dblock.capacity() < ret)
+            return ZCE_ERROR_MALLOC;
+        ret = zdp::zds_pack_builtin(dblock.rd_ptr(), (int)dblock.capacity(), t, 0);
+        if (ret < 0)
+            return ret;
+        dblock.wr_ptr(ret);
+        return rpc_call_dblock(vmptr, method, dblock, response);
+    }
+
+    template<typename T>
     int rpc_call(const zce_smartptr<zce_object>& vmptr,
         const std::string& method,
         const T& t,
