6fefd137e3e9743033e9199b2b5fb1c24bf011be
wangym <wangym@gmail.com>
2024-01-02 06:48:56 +0800

    rpcdemo is external

--- RpcDemo/src/rpcdemo_config.cpp
+++
@@ -1,9 +0,0 @@
-#include "rpcdemo_inc.h"
-#include "rpcdemo_config.h"
-#include "rpcdemo_config_pack.h"
-
-int rpcdemo_config::process_content(unsigned char* xml, unsigned length)
-{
-    int ret = rpcdemo::zxml_unpack(conf_, xml, length, 0);
-    return ret;
-}
