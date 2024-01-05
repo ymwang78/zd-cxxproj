e948b8dca10b6c8b0e74a1c9fee4cbdeed94c0cd
Yongming Wang <wangym@gmail.com>
2024-01-05 13:29:34 +0800

    allocator subclass

--- include/zce/zce_allocator.h
+++ include/zce/zce_allocator.h
@@ -20,18 +20,36 @@
 
 struct ZCE_API zce_alloc_stat {
 
-    zce_alloc_stat(size_t chunk_s, size_t n_chunks);
+    zce_alloc_stat(size_t chunk_s);
 
-    unsigned chunk_count_;          //how many chunks in one pool
-    unsigned chunk_size_;           //the size of chunk
-    unsigned chunk_total_;          //total chunks in all of the pool
+    zce_alloc_stat() = default;
+
+    inline unsigned current_alloc() const noexcept {
+        return chunk_alloc_times_ - chunk_free_times_;
+    }
+
+    inline void set_max_alloc() noexcept {
+        unsigned cur_alloc = current_alloc();
+        if (cur_alloc > chunk_max_used_)
+            chunk_max_used_ = cur_alloc;
+    }
+
+    unsigned chunk_data_size_;           //the size of chunk
     unsigned chunk_availble_;       //how many chunks available for use
     unsigned chunk_alloc_times_;    //alloc times, for profile
     unsigned chunk_free_times_;     //free times, for profile
-    unsigned chunk_max_alloc_;      //the max number of chunks used in history
+    unsigned chunk_max_used_;      //the max number of chunks used in history
 };
 
 class ZCE_API zce_allocator : public zce_object
+{
+public:
+    virtual void* alloc(size_t nbytes, size_t* nreal) = 0;
+
+    virtual void zfree(void* item) = 0; //fuck off the macro
+};
+
+class ZCE_API zce_allocator_chunk : public zce_allocator
 {
 
 private:
@@ -40,6 +58,8 @@ private:
     zce_mutex pool_lock_;           //protected alloc and free if lock is enabled
     
     unsigned long work_threadid_;   //debug purpose only
+
+    unsigned chunk_count_;          //how many chunks in one pool
 
     zce_alloc_stat alloc_stat_;
 
@@ -55,19 +75,19 @@ private:
 
 public:
 
-    inline unsigned chunk_size() const noexcept { return alloc_stat_.chunk_size_; }
+    inline unsigned chunk_size() const noexcept { return alloc_stat_.chunk_data_size_; }
 
     inline zce_alloc_stat stat() const noexcept { return alloc_stat_; }
 
 public:
 
-    zce_allocator(size_t chunk_size, size_t n_chunks, bool lock);
+    zce_allocator_chunk(size_t chunk_size, size_t n_chunks, bool lock);
 
-    virtual ~zce_allocator();
+    virtual ~zce_allocator_chunk();
 
-    void *alloc(size_t nbytes, size_t* nreal);
+    void *alloc(size_t nbytes, size_t* nreal) override;
 
-    void zfree(void* item); //fuck off the macro
+    void zfree(void* item) override; //fuck off the macro
 };
 
 #endif // __zce_allocator_h__
