34abbb95cda83758969bf58cbdc51a339e22bb91
wangym <wangym@gmail.com>
2024-01-22 19:01:28 +0800

    f

--- RpcDemo/src/slots_way.cpp
+++ RpcDemo/src/slots_way.cpp
@@ -17,12 +17,12 @@ int NCR(int n, int r)
      using property:
      N choose R = N choose (N-R)
     */
-    if (r > n / 2) return NCR(n, n - r);
+    if (r > n / 2)
+        r = n - r;
 
     long res = 1;
 
-    for (int k = 1; k <= r; ++k)
-    {
+    for (int k = 1; k <= r; ++k) {
         res *= n - k + 1;
         res /= k;
     }
@@ -60,19 +60,19 @@ static int_matrix _peifu_matrix = {
 };
 
 static int_matrix _relative_expect_matrix = {
-    { 2000, 2000, 2000, 2000, 2000, 2000 },
-    { 2000, 2000, 2000, 2000, 2000, 2000},
-    { 2000, 2000, 2000, 2000, 2000, 2000},
-    { 800, 800, 800, 800, 800, 800},
-    { 800, 800, 800, 800, 800, 800},
-    { 700, 700, 700, 700, 700, 700},
-    { 700, 700, 700, 700, 700, 700},
-    { 600, 600, 600, 600, 600, 600},
-    { 400, 400, 400, 400, 400, 400},
-    { 300, 300, 300, 300, 300, 300},
-    { 200, 200, 200, 200, 200, 200},
-    { 100, 100, 100, 100, 100, 100},
-    { 100, 100, 100, 100, 100, 100}
+    { 1000, 1000, 1000, 1000, 1000, 1000 },
+    { 1000, 1000, 1000, 1000, 1000, 1000 },
+    { 1000, 1000, 1000, 1000, 1000, 1000 },
+    { 800, 800, 800, 800, 800, 800 },
+    { 800, 800, 800, 800, 800, 800 },
+    { 700, 700, 700, 700, 700, 700 },
+    { 700, 700, 700, 700, 700, 700 },
+    { 600, 600, 600, 600, 600, 600 },
+    { 400, 400, 400, 400, 400, 400 },
+    { 300, 300, 300, 300, 300, 300 },
+    { 200, 200, 200, 200, 200, 200 },
+    { 100, 100, 100, 100, 100, 100 },
+    { 100, 100, 100, 100, 100, 100 }
 };
 
 template<typename T>
@@ -151,39 +151,42 @@ static double _calc_rtp_n(const dbl_vector& p_vec, const dbl_vector& notp_vec, c
     ZCE_ASSERT_RETURN(pengcnt >= 3 && pengcnt <= 6, 0);
     //第一列i个，第二列j个，第三列k个，第四列l个，第五列m个，第六列n个的概率*倍数*赔付
     double rtp = 0;
-    for (int i = 0; i < _row_num[0]; ++i) {
-        for (int j = 0; j < _row_num[1]; ++j) {
-            for (int k = 0; k < _row_num[2]; ++k) {
+    for (int i = 1; i <= _row_num[0]; ++i) {
+        for (int j = 1; j <= _row_num[1]; ++j) {
+            for (int k = 1; k <= _row_num[2]; ++k) {
                 if (pengcnt == 3) {
-                    rtp += NCR(_row_num[0], i + 1) * pow(p_vec[0], i + 1) * i
-                        * NCR(_row_num[1], j + 1) * pow(p_vec[1], j + 1) * j
-                        * NCR(_row_num[2], k + 1) * pow(p_vec[2], k + 1) * k;
+                    rtp += NCR(_row_num[0], i) * pow(p_vec[0], i) * pow(notp_vec[0], _row_num[0] - i) * i
+                        * NCR(_row_num[1], j) * pow(p_vec[1], j) * pow(notp_vec[1], _row_num[1] - j) * j
+                        * NCR(_row_num[2], k) * pow(p_vec[2], k) * pow(notp_vec[2], _row_num[2] - k) * k
+                        * notp_vec[3];
                 }
                 else {
-                    for (int l = 0; l < _row_num[3]; ++l) {
+                    for (int l = 1; l <= _row_num[3]; ++l) {
                         if (pengcnt == 4) {
-                            rtp += NCR(_row_num[0], i + 1) * pow(p_vec[0], i + 1) * i
-                                * NCR(_row_num[1], j + 1) * pow(p_vec[1], j + 1) * j
-                                * NCR(_row_num[2], k + 1) * pow(p_vec[2], k + 1) * k
-                                * NCR(_row_num[3], l + 1) * pow(p_vec[3], l + 1) * l;
+                            rtp += NCR(_row_num[0], i) * pow(p_vec[0], i) * pow(notp_vec[0], _row_num[0] - i) * i
+                                * NCR(_row_num[1], j) * pow(p_vec[1], j) * pow(notp_vec[1], _row_num[1] - j) * j
+                                * NCR(_row_num[2], k) * pow(p_vec[2], k) * pow(notp_vec[2], _row_num[2] - k) * k
+                                * NCR(_row_num[3], l) * pow(p_vec[3], l) * pow(notp_vec[3], _row_num[3] - l) * l
+                                * notp_vec[4];
                         }
                         else {
-                            for (int m = 0; m < _row_num[4]; ++m) {
+                            for (int m = 1; m <= _row_num[4]; ++m) {
                                 if (pengcnt == 5) {
-                                    rtp += NCR(_row_num[0], i + 1) * pow(p_vec[0], i + 1) * i
-                                        * NCR(_row_num[1], j + 1) * pow(p_vec[1], j + 1) * j
-                                        * NCR(_row_num[2], k + 1) * pow(p_vec[2], k + 1) * k
-                                        * NCR(_row_num[3], l + 1) * pow(p_vec[3], l + 1) * l
-                                        * NCR(_row_num[4], m + 1) * pow(p_vec[4], m + 1) * m;
+                                    rtp += NCR(_row_num[0], i) * pow(p_vec[0], i) * pow(notp_vec[0], _row_num[0] - i) * i
+                                        * NCR(_row_num[1], j) * pow(p_vec[1], j) * pow(notp_vec[1], _row_num[1] - j) * j
+                                        * NCR(_row_num[2], k) * pow(p_vec[2], k) * pow(notp_vec[2], _row_num[2] - k) * k
+                                        * NCR(_row_num[3], l) * pow(p_vec[3], l) * pow(notp_vec[3], _row_num[3] - l) * l
+                                        * NCR(_row_num[4], m) * pow(p_vec[4], m) * pow(notp_vec[4], _row_num[4] - m) * m
+                                        * notp_vec[5];
                                 }
                                 else {
-                                    for (int n = 0; n < _row_num[5]; ++n) {
-                                        rtp += NCR(_row_num[0], i + 1) * pow(p_vec[0], i + 1) * i
-                                            * NCR(_row_num[1], j + 1) * pow(p_vec[1], j + 1) * j
-                                            * NCR(_row_num[2], k + 1) * pow(p_vec[2], k + 1) * k
-                                            * NCR(_row_num[3], l + 1) * pow(p_vec[3], l + 1) * l
-                                            * NCR(_row_num[4], m + 1) * pow(p_vec[4], m + 1) * m
-                                            * NCR(_row_num[5], n + 1) * pow(p_vec[5], n + 1) * n;
+                                    for (int n = 1; n <= _row_num[5]; ++n) {
+                                        rtp += NCR(_row_num[0], i) * pow(p_vec[0], i) * pow(notp_vec[0], _row_num[0] - i) * i
+                                            * NCR(_row_num[1], j) * pow(p_vec[1], j) * pow(notp_vec[1], _row_num[1] - j) * j
+                                            * NCR(_row_num[2], k) * pow(p_vec[2], k) * pow(notp_vec[2], _row_num[2] - k) * k
+                                            * NCR(_row_num[3], l) * pow(p_vec[3], l) * pow(notp_vec[3], _row_num[3] - l) * l
+                                            * NCR(_row_num[4], m) * pow(p_vec[4], m) * pow(notp_vec[4], _row_num[4] - m) * m
+                                            * NCR(_row_num[5], n) * pow(p_vec[5], n) * pow(notp_vec[5], _row_num[5] - n) * n;
                                     }
                                 }
                             }
@@ -208,9 +211,12 @@ static dbl_vector _calc_rtp_x(const dbl_matrix& p_matrix, const dbl_matrix& notp
     return (rtp_vec);
 }
 
-void calc_rtp()
+void calc_rtp_matrix(const int_matrix& matrix)
 {
-    dbl_matrix p_matrix = _calc_p_matrix(_relative_expect_matrix);
+    std::cout << "---------expect---------" << std::endl;
+    print_vec(matrix);
+
+    dbl_matrix p_matrix = _calc_p_matrix(matrix);
     std::cout << "---------p---------" << std::endl;
     print_vec(p_matrix);
 
@@ -232,4 +238,36 @@ void calc_rtp()
 
     std::cout << "---------sum---------" << std::endl;
     std::cout << zce_sum(rtp_vec) << std::endl << std::endl << std::endl;
+}
+
+static int_matrix _replace(const int_matrix& in, int ival, int rval)
+{
+    int_matrix new_matrix(in.size());
+    for (int i = 0; i < in.size(); ++i) {
+        new_matrix[i].resize(in[i].size());
+        for (int j = 0; j < in[i].size(); ++j) {
+            new_matrix[i][j] = (in[i][j] == ival) ? rval : in[i][j];
+        }
+    }
+    return new_matrix;
+}
+
+void calc_rtp()
+{
+    //calc_rtp_matrix(_relative_expect_matrix);
+
+    int_matrix i2000 = _replace(_relative_expect_matrix, 1000, 1000);
+    calc_rtp_matrix(i2000);
+
+    //int_matrix i800 = _replace(_relative_expect_matrix, 1000, 800);
+    //calc_rtp_matrix(i800);
+
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << NCR(5, i) << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 1; i <= 6; ++i) {
+        std::cout << NCR(6, i) << " ";
+    }
+    std::cout << std::endl;
 }
