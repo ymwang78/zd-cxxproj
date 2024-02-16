d11ad3b7354ed1ae9688619d5bbdf1e161d63952
wangym <wangym@gmail.com>
2024-02-16 13:19:30 +0800

    zce file

--- include/zce/zce_filesystem.h
+++ include/zce/zce_filesystem.h
@@ -15,6 +15,22 @@
 #include <stdarg.h>
 #include <sstream>
 
+#ifdef __LINUX__
+#   include <linux/fs.h>
+#   define ZCE_OPEN_RDFLAG      (O_NOATIME | O_RDONLY | O_LARGEFILE)		/* only for mount, need not O_DIRECT */
+#   define ZCE_OPEN_WRFLAG      (O_WRONLY | O_NOATIME | O_LARGEFILE)		/* only for format, need not O_DIRECT */
+#   define ZCE_OPEN_RDWRFLAG    (O_RDWR | O_NOATIME | O_LARGEFILE)
+#   define ZCE_OPEN_CRWFLAG     (O_RDWR | O_CREAT | O_NOATIME | O_LARGEFILE)
+#elif defined(_WIN32)
+#   include <fcntl.h>
+#   define ZCE_OPEN_RDFLAG      (0)
+#   define ZCE_OPEN_WRFLAG      (O_WRONLY)
+#   define ZCE_OPEN_RDWRFLAG    (O_RDWR)
+#   define ZCE_OPEN_CRWFLAG     (O_RDWR | O_CREAT)
+#else
+#   error "unsupport platform"
+#endif
+
 std::string ZCE_API zce_get_path(const char* filename);
 
 std::string ZCE_API zce_abs_path(const char* filename);
@@ -68,9 +84,11 @@ public:
 
     ssize_t writev(const iovec* iov, int iovcnt);
 
-    int lseek_blocks(zce_uint64 block_id);
+    int lseek_block(zce_uint64 block_id);
 
-    int lseek(zce_uint64 pos);
+    int lseek(zce_uint64 pos, int whence = SEEK_SET);
 
     int ftruncate(zce_int64 newsize);
+
+    zce_int64 size();
 };
--- include/zce/zce_list.h
+++ include/zce/zce_list.h
@@ -57,6 +57,58 @@ static inline void zce_list_del(struct zce_list_node *entry)
     __list_del(entry->prev, entry->next);
 };
 
+inline zce_list_node* zce_list_del_head(zce_list_node* head)
+{
+    zce_list_node* p = head->next;
+    zce_list_del(p);
+    return p;
+}
+
+inline zce_list_node* zce_list_del_tail(zce_list_node* head)
+{
+    zce_list_node* p = head->prev;
+    zce_list_del(p);
+    return p;
+}
+
+inline void __list_splice(const zce_list_node* list, zce_list_node* prev, zce_list_node* next)
+{
+    zce_list_node* first = list->next;
+    zce_list_node* last = list->prev;
+    first->prev = prev;
+    prev->next = first;
+    last->next = next;
+    next->prev = last;
+}
+
+inline void zce_list_splice(const zce_list_node* list, zce_list_node* head)
+{
+    if (!zce_list_empty(list))
+        __list_splice(list, head, head->next);
+}
+
+inline void zce_list_splice_tail(zce_list_node* list, zce_list_node* head)
+{
+    if (!zce_list_empty(list))
+        __list_splice(list, head->prev, head);
+}
+
+inline void zce_list_splice_init(zce_list_node* list, zce_list_node* head)
+{
+    if (!zce_list_empty(list)) {
+        __list_splice(list, head, head->next);
+        INIT_LIST_HEAD(list);
+    }
+}
+
+inline void zce_list_splice_tail_init(zce_list_node* list, zce_list_node* head)
+{
+    if (!zce_list_empty(list)) {
+        __list_splice(list, head->prev, head);
+        INIT_LIST_HEAD(list);
+    }
+}
+
 #define zce_container_of(ptr, type, member) \
   ((type *) ((char *) (ptr) - offsetof(type, member)))
 
@@ -78,6 +130,9 @@ static inline void zce_list_del(struct zce_list_node *entry)
 #define zce_list_for_each_safe(pos, n, head) \
     for (pos = (head)->next, n = pos->next; pos != (head); \
         pos = n, n = pos->next)
+
+#define zce_list_for_each_prev(pos, head) \
+    for (pos = (head)->prev; pos != (head); pos = pos->prev)
 
 #define zce_list_for_each_prev_safe(pos, n, head) \
     for (pos = (head)->prev, n = pos->prev; \
