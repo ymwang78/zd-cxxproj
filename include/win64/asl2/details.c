b0a86576538842e86c6bfe8f3b3b92346e1ac1ee
Yongming <wangym@gmail.com>
2025-06-06 13:54:23 +0800

    提交asl2 ipopt 头文件

---
+++ include/win64/asl2/details.c
@@ -0,0 +1,5 @@
+/* For operating-system and compiler details...   */
+/* Obtain details.c by changeing "Windows AMD64" */
+/* to the appropriate string. */
+
+char sysdetails_ASL[] = "Windows AMD64";
