4784b8f32c44dbe74b556178611dc0f3833b67b5
Yongming <wangym@gmail.com>
2025-12-22 09:48:02 +0800

    fix zvm

--- include/zce/zvm.h
+++ include/zce/zvm.h
@@ -58,7 +58,8 @@ class VirtualMachineStub : public zce::Object {
                                     zce::RefBlock args);
 
     zce::SmartPtr<zce::Object> boot(const std::string& svc_name, const std::string& host,
-                                    unsigned short port, bool ssl, int default_timeout);
+                                    unsigned short port, bool ssl, int default_timeout,
+                                    std::function<void()> close_cb);
 
     zce::SmartPtr<zce::Object> get_vm(const std::string& svc_name) const;
 
