99af5614d46aafcee57635a8b0e46c43b8bbce7c
Yongming <wangym@gmail.com>
2025-12-25 15:28:02 +0800

    EnableASAN NO

--- vcpkg_requirements.bat
+++ vcpkg_requirements.bat
@@ -1,5 +1,6 @@
 rem 设置环境变量 VCPKG_ROOT=D:\Github\vcpkg
 rem .\vcpkg integrate install
+
 vcpkg install boost-algorithm:x64-windows-static-md 
 vcpkg install boost-tokenizer:x64-windows-static-md 
 vcpkg install boost-lexical-cast:x64-windows-static-md
@@ -17,13 +18,11 @@ vcpkg install nlohmann-json:x64-windows-static-md
 vcpkg install lua:x64-windows-static-md 
 vcpkg install gtest:x64-windows-static-md
 vcpkg install hiredis:x64-windows-static-md
-rem vcpkg install cxxopts:x64-windows-static-md 
-rem vcpkg install coin-or-ipopt:x64-windows-static-md
-rem vcpkg install mongo-cxx-driver:x64-windows-static-md
-rem TaijiMPC client requirements
+
+vcpkg install lua:x64-windows-static
+
 vcpkg install bzip2:x86-windows-static
 vcpkg install open62541:x86-windows-static
 vcpkg install openssl:x86-windows-static
 vcpkg install boost-algorithm:x86-windows-static
 vcpkg install boost-tokenizer:x86-windows-static
-vcpkg install lua:x64-windows-static
