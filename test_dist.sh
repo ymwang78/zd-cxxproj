9a8997965747f3fcb32ab3c72d9dc962386110fb
Yongming Wang <wangym@gmail.com>
2025-12-28 04:01:16 -0500

    fix

--- test_dist.sh
+++ test_dist.sh
@@ -46,5 +46,5 @@ else
 fi
 
 cp HostVM/dist/docker/*.xml /opt/hostvm/hostvm.xml
-cp HostVM/dist/docker/*.ptl /opt/hostvm/
+#cp HostVM/dist/docker/*.ptl /opt/hostvm/
 cp HostVM/dist/docker/*.py /opt/hostvm/
